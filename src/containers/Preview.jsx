b23a9b2c64a59b96bafd2a1325161f7a5b2f27dd
Django Cass <dj.cass44@gmail.com>
2019-09-15 15:44:20 +1000

    Added JMP api health query Added license

--- src/containers/Preview.jsx
+++ src/containers/Preview.jsx
@@ -1,7 +1,54 @@
-import React from "react";
-import {Code, Table} from "evergreen-ui";
+/*
+ *    Copyright 2019 Django Cass
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+import React, {useEffect, useState} from "react";
+import {Code, Icon, Spinner, Table, Tooltip} from "evergreen-ui";
+import getHealth from "../util/getHealth";
 
 export default () => {
+	const [status, setStatus] = useState(null);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		setError(null);
+		fetch("http://localhost:7000/api/v3/health").then(r => {
+			if (!r.ok)
+				throw new Error(`HTTP error, status = ${r.status}`);
+			return r.json();
+		}).then(data => {
+			setStatus(data);
+		}).catch(err => {
+			setError(err.message);
+			console.log(err);
+		});
+	}, []);
+
+	const getStatus = () => {
+		if (error != null)
+			return error;
+		if (status != null)
+			return getHealth(status).map(i => (
+				<Tooltip content={i.key}>
+					<Icon margin={2} icon={i.value === true ? "tick-circle" : "ban-circle"}
+					      color={i.value === true ? "success" : "danger"}/>
+				</Tooltip>
+			));
+		return <Spinner size={16}/>;
+	};
+
 	const rows = [
 		{
 			key: "URL",
@@ -9,14 +56,14 @@ export default () => {
 		},
 		{
 			key: "App status",
-			value: "OK"
+			value: getStatus()
 		}
 	];
 	return (
 		<Table minWidth={350}>
 			<Table.Body>
 				{rows.map(r => (
-					<Table.Row>
+					<Table.Row key={r.key}>
 						<Table.TextCell maxWidth={90}>{r.key}</Table.TextCell>
 						<Table.TextCell>{r.value}</Table.TextCell>
 					</Table.Row>
---
+++ src/util/getHealth.js
@@ -0,0 +1,33 @@
+/*
+ *    Copyright 2019 Django Cass
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+export default health => {
+	const results = [];
+	if (health == null) return results;
+	results.push({key: "HTTP status code", value: health.code === 200});
+	results.push({key: "Content server", value: health.http === "OK"});
+	// database can be null (if not admin)
+	if (health.database != null)
+		results.push({key: "Database", value: health.database});
+	// identity can be null
+	if (health.identityProvider != null)
+		results.push({key: "Identity Provider", value: health.identityProvider});
+	// imageApi is optional
+	if (health.imageApi != null)
+		results.push({key: "Image Api", value: health.imageApi});
+	return results;
+};
