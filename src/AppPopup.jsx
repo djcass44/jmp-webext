a58d080f8a9018a3a007cc6cd80c373d9eecc58e
Django Cass <dj.cass44@gmail.com>
2019-09-15 15:16:01 +1000

    Added initial popup

--- src/AppPopup.jsx
+++ src/AppPopup.jsx
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './AppPopup.css';
+import {Card, Code, Heading, IconButton, Pane, Table, Tooltip} from "evergreen-ui";
 
 export default () => {
 	const [color, setColor] = useState("");
@@ -13,26 +14,40 @@ export default () => {
 		setColor(data.color);
 	};
 
-	const handleButtonClick = async () => {
-		const tabs = await browser.tabs.query({
-			active: true,
-			currentWindow: true
-		});
-		browser.tabs.executeScript(
-			tabs[0].id,
-			{
-				code: `document.body.style.backgroundColor = "${color}";`
-			}
-		);
-	};
+	const rows = [
+		{
+			key: "URL",
+			value: <Code>"https://jmp.castive.dev"</Code>
+		},
+		{
+			key: "App status",
+			value: "OK"
+		}
+	];
+
 	return (
-		<div className="App">
-			<button
-				onClick={handleButtonClick}
-				style={{
-					backgroundColor: color
-				}}
-			/>
-		</div>
+		<Pane padding={12}>
+			<Heading size={700} padding={8}>JMP</Heading>
+			<Card elevation={1}>
+				<Table minWidth={350}>
+					<Table.Body>
+						{rows.map(r => (
+							<Table.Row>
+								<Table.TextCell maxWidth={90}>{r.key}</Table.TextCell>
+								<Table.TextCell>{r.value}</Table.TextCell>
+							</Table.Row>
+						))}
+					</Table.Body>
+				</Table>
+			</Card>
+			<Pane padding={12} display="flex">
+				<Tooltip content="Open JMP">
+					<IconButton appearance="minimal" icon="document-open"/>
+				</Tooltip>
+				<Tooltip content="Settings">
+					<IconButton appearance="minimal" icon="settings"/>
+				</Tooltip>
+			</Pane>
+		</Pane>
 	);
 }
--- src/options.js
+++ src/options.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppOptions from './AppOptions';
+import "typeface-roboto";
 
 ReactDOM.render(
 	<AppOptions />,
