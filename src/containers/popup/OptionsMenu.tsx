225a1855f79f398b8ca74990ed7312a8b5bdd88b
Django Cass <django@dcas.dev>
2020-03-14 21:17:30 +1100

    Fixed menu links not working in Chrome

--- src/containers/popup/OptionsMenu.tsx
+++ src/containers/popup/OptionsMenu.tsx
@@ -1,32 +1,34 @@
-import {Link, Menu} from "evergreen-ui";
+import {Menu} from "evergreen-ui";
 import React from "react";
 
 interface OptionsMenuProps {
 	url?: string | null;
 }
 
-const linkStyle = {
-	textDecoration: "none",
-	color: "#101f2d"
-};
-
 const OptionsMenu: React.FC<OptionsMenuProps> = ({url}) => {
 
-	const getPath = (path: string): string => {
-		let newUrl;
+	const openTab = (target: string): void => {
+		browser.tabs.create({
+			url: target,
+			active: true
+		}).catch((err: any) => console.error(err));
+	};
+
+	const openPath = (path: string) => {
+		let settingsUrl;
 		if(url && url.endsWith("/"))
-			newUrl = `${url}${path}`;
+			settingsUrl = `${url}${path}`;
 		else
-			newUrl = `${url}/${path}`;
-		return newUrl;
+			settingsUrl = `${url}/${path}`;
+		openTab(settingsUrl);
 	};
 
 	return (
 		<Menu>
 			<Menu.Group title="JMP Actions">
-				<Menu.Item is={Link} style={linkStyle} href={url}>JMP</Menu.Item>
-				<Menu.Item is={Link} style={linkStyle} href={getPath("settings")}>JMP Options</Menu.Item>
-				<Menu.Item is={Link} style={linkStyle} href={getPath("help")}>JMP Help</Menu.Item>
+				<Menu.Item onSelect={() => openPath("")}>JMP</Menu.Item>
+				<Menu.Item onSelect={() => openPath("settings")}>JMP Options</Menu.Item>
+				<Menu.Item onSelect={() => openPath("help")}>JMP Help</Menu.Item>
 			</Menu.Group>
 			<Menu.Divider/>
 			<Menu.Group title="Extension Actions">
